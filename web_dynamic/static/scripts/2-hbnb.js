19fe32d6b13f0fd2eea184fecc343f4ede51d124
KingJ89 <janmutewera@gmail.com>
2024-08-11 16:35:53 -0700

    Task 2 web_dynamic completed

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,5 +14,14 @@ $(document).ready(function () {
         }
         $('.amenities h4').html(amenityNames);
     });
+
+    // Check API status
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
 });
 
